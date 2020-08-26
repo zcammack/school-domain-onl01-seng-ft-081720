3ff001cba9e7d78ad23683fd6fcb8a528a9efba3
Zackary Cammack <zack.cammack@gmail.com>
2020-08-26 20:34:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,8 +6,16 @@ class School
     @roster = {}
   end
 
-  def add_student(name, grade)
+  def add_student(student_name, grade)
     roster[grade] ||= []
-    roster[grade] << name
+    roster[grade] << student_name
   end
+
+  def grade(grade)
+    roster.detect do |a, b|
+      if a == grade
+        return b
+      end
+    end
+  end 
 end
