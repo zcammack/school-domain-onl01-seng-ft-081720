a4f149b27e1f07ee9950708d0db684fce30aa4c1
Zackary Cammack <zack.cammack@gmail.com>
2020-08-26 20:54:07 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+  def grade(grade)
+    roster.detect do |a, b|
+      if a == grade
+        return b
+      end
+    end
+  end
+
+  def sort
+    sorted_roster = {}
+    roster.each do |a, b|
+      sorted_roster[a] = b.sort
+    end
+    sorted_roster
+  end
+end
